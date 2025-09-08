7b4a0a4e09f0d545f55f14b67de57a468494734e
bearxiao9756 <bearxiao9@gmail.com>
2025-09-08 15:55:15 +0800

    Update gateway_client.go

--- app/interface/gnetway/client/gateway_client.go
+++ app/interface/gnetway/client/gateway_client.go
@@ -12,6 +12,7 @@ package gateway_client
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/teamgram/proto/mtproto"
 	"github.com/teamgram/teamgram-server/app/interface/gnetway/gateway"
@@ -38,6 +39,7 @@ func NewGatewayClient(cli zrpc.Client) GatewayClient {
 // GatewaySendDataToGateway
 // gateway.sendDataToGateway auth_key_id:long session_id:long payload:bytes = Bool;
 func (m *defaultGatewayClient) GatewaySendDataToGateway(ctx context.Context, in *gateway.TLGatewaySendDataToGateway) (*mtproto.Bool, error) {
+	fmt.Printf("[GatewaySendDataToGateway] Sending data to gateway for auth_key_id: %d, session_id: %d\n", in.GetAuthKeyId(), in.GetSessionId())
 	client := gateway.NewRPCGatewayClient(m.cli.Conn())
 	return client.GatewaySendDataToGateway(ctx, in)
 }
